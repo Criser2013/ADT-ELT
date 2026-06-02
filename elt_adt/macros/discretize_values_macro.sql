bac1718eaa26d4b015c741693e992858f2c044ec
Cristian O. <84862634+Criser2013@users.noreply.github.com>
2026-06-02 09:24:38 -0500

    Gold model designed to be used as train data implemented.

---
+++ elt_adt/macros/discretize_values_macro.sql
@@ -0,0 +1,9 @@
+{%- macro discretize_values(column, intervals, lower_bound = 0, lower_value = -1, upper_value = 999) -%}
+    case
+        {%- for lower, upper, value in intervals -%}
+            when ({{ column }} >= {{ lower }}) and ({{ column }} < {{ upper }}) then {{ value }}
+        {%- endfor -%}
+        when {{ column }} < {{ lower_bound }} then {{ lower_value }}
+        else {{ upper_value }}
+    end
+{%- endmacro -%}
